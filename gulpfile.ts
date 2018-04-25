2c8e6df73fed9c3c65400ee1bb2710c7c8593296
vineeth soma <vineeth.soma@esadvisory.com>
2018-04-25 19:40:36 -0400

    gulpfile added

---
+++ gulpfile.ts
@@ -0,0 +1,12 @@
+import * as gulp from "gulp";
+import * as mocha from "gulp-mocha"; 
+import * as env from "gulp-env"; 
+
+gulp.task('test', () => {
+    console.log("Testing..");
+}); 
+
+
+const compileTS = (args) => {
+
+}
---
+++ server/test/client/client-tests.ts
@@ -0,0 +1,6 @@
+import * as chai from "chai";
+import * as Configs from "../../src/configurations";
+import * as Server from "../../src/server";
+
+const assert = chai.assert;
+const serverConfig = Configs.getServerConfigs();
