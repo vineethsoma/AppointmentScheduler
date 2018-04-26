37e10f67bb519315da48077186c2c7c1d1d447ef
vineeth soma <vineeth.soma@esadvisory.com>
2018-04-25 22:11:06 -0400

    added shared exports and material styling

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -1,9 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {
+  MatCardModule,
+  MatButtonModule,
+  MatDialogModule
+} from '@angular/material';
 
 @NgModule({
   imports: [
     CommonModule
+  ],
+  exports: [
+    MatCardModule
   ],
   declarations: []
 })
